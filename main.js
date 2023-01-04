51141585c900e98a31c211ab484b34afcdf5e234
MightyTiger <47121543+MightyT1ger@users.noreply.github.com>
2023-01-04 21:28:23 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 function sendEmail() {
   Email.send({
-    Host : "smtp.gmail.com",
-    Username : "skripteditor@gmail.com",
-    Password : "#Krokodil2005",
+    SecureToken : "24e2a136-ced9-410c-ba16-f9d5fb51ac68",
     To : 'skripteditor@gmail.com',
     From : document.getElementById("email").value,
     Subject : document.getElementById("subject").value,
